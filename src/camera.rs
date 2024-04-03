148fef891488dc24d899d51be57593d21fd845a4
Javier Lancha Vázquez <javier.lancha@gmail.com>
2024-04-03 22:34:26 +0200

    Implement camera aperture and focus distance

--- src/camera.rs
+++ src/camera.rs
@@ -19,6 +19,8 @@
 
 use std::f32::consts::PI;
 
+use rand::{rngs::ThreadRng, Rng};
+
 use crate::algebra::{self, Vec3, UNIT_Y};
 use crate::light::Ray;
 
@@ -34,6 +36,18 @@ impl Default for FieldOfView {
     }
 }
 
+#[derive(Debug, Clone, Copy)]
+pub enum FocusMode {
+    FocalPlane { focal_distance: f32, aperture: f32 },
+    PinHole,
+}
+
+impl Default for FocusMode {
+    fn default() -> Self {
+        Self::PinHole
+    }
+}
+
 #[derive(Default, Debug)]
 pub struct CameraConfig {
     pub position: Vec3,
@@ -41,6 +55,7 @@ pub struct CameraConfig {
     pub resolution: (usize, usize),
     pub rotation: f32,
     pub fov: FieldOfView,
+    pub focus_mode: FocusMode,
 }
 
 #[derive(Default, Debug)]
@@ -57,6 +72,7 @@ pub struct Camera {
     rotation: f32,                       // Rotation, in the positive sense, around the facing axis
     resolution: (usize, usize),          // Resolutions (width, height) in pixels
     fov: FieldOfView,                    // Field of view (Horizontal or Vertical) in radians
+    focus_mode: FocusMode,
 
     distance_to_plane: f32,
     first_pixel_pos: Vec3,
@@ -97,17 +113,36 @@ impl Camera {
         self.resolution
     }
 
-    pub fn config(&mut self, config: &CameraConfig) {
+    pub fn config(&mut self, config: &CameraConfig) -> Result<(), String> {
         const WORLD_UP: Vec3 = UNIT_Y;
 
-        self.resolution = config.resolution;
-        assert!(
-            ((self.resolution.0 * self.resolution.1) > 0),
-            "The camera resolution cannot be zero"
-        );
+        if (config.resolution.0 * config.resolution.1) == 0 {
+            return Err("The camera resolution cannot be zero".to_string());
+        }
 
+        match config.fov {
+            FieldOfView::Horizontal(mut alpha) => {
+                alpha = alpha.abs();
+                if alpha <= 0.0 || alpha > PI {
+                    return Err(
+                        "Horizontal field of view must be in the interval (0, 180)".to_string()
+                    );
+                }
+                self.fov = FieldOfView::Horizontal(alpha);
+            }
+            FieldOfView::Vertical(mut alpha) => {
+                alpha = alpha.abs();
+                if alpha <= 0.0 || alpha > PI {
+                    return Err(
+                        "Vertical field of view must be in the interval (0, 180)".to_string()
+                    );
+                }
+                self.fov = FieldOfView::Horizontal(alpha);
+            }
+        }
+
+        self.resolution = config.resolution;
         self.rotation = config.rotation;
-        self.fov = config.fov;
 
         // Set coordinates
         self.coordinate_system.w = config.direction.normal();
@@ -134,36 +169,44 @@ impl Camera {
             self.coordinate_system.v.normalize();
         }
 
-        // Calculate distance to plane using fov
+        // Calculate distance to plane using fov and focus parameters
         let aspect_ratio: f32 = (self.resolution.0 as f32) / (self.resolution.1 as f32);
-        let sensor_height: f32 = 1.0;
-        let sensor_width: f32 = sensor_height * aspect_ratio;
-        self.distance_to_plane = match self.fov {
-            FieldOfView::Horizontal(mut alpha) => {
-                alpha = alpha.abs();
-                assert!(
-                    alpha > 0.0,
-                    "Horizontal field of view must be greater than 0 degrees"
-                );
-                assert!(
-                    alpha < PI,
-                    "Horizontal field of view must be smaller than 180 degrees"
-                );
-                sensor_width / (2.0 * (alpha / 2.0).tan())
+        let (sensor_width, sensor_height): (f32, f32) = match config.focus_mode {
+            FocusMode::FocalPlane {
+                focal_distance,
+                aperture,
+            } => {
+                if focal_distance < 0.0 {
+                    return Err("Focal distance must be postive".to_string());
+                } else if aperture < 0.0 {
+                    return Err("Aperture must be postive".to_string());
+                }
+
+                self.distance_to_plane = focal_distance;
+                match self.fov {
+                    FieldOfView::Horizontal(alpha) => {
+                        let sensor_width = 2.0 * self.distance_to_plane * ((alpha / 2.0).tan());
+                        let sensor_height = sensor_width / aspect_ratio;
+                        (sensor_width, sensor_height)
+                    }
+                    FieldOfView::Vertical(alpha) => {
+                        let sensor_height = 2.0 * self.distance_to_plane * ((alpha / 2.0).tan());
+                        let sensor_width = sensor_height * aspect_ratio;
+                        (sensor_width, sensor_height)
+                    }
+                }
             }
-            FieldOfView::Vertical(mut alpha) => {
-                alpha = alpha.abs();
-                assert!(
-                    alpha > 0.0,
-                    "Vertical field of view must be greater than 0 degrees"
-                );
-                assert!(
-                    alpha < PI,
-                    "Vertical field of view must be smaller than 180 degrees"
-                );
-                sensor_height / (2.0 * (alpha / 2.0).tan())
+            FocusMode::PinHole => {
+                let sensor_height = 1.0;
+                let sensor_width = sensor_height * aspect_ratio;
+                self.distance_to_plane = match self.fov {
+                    FieldOfView::Horizontal(alpha) => sensor_width / (2.0 * (alpha / 2.0).tan()),
+                    FieldOfView::Vertical(alpha) => sensor_height / (2.0 * (alpha / 2.0).tan()),
+                };
+                (sensor_width, sensor_height)
             }
         };
+        self.focus_mode = config.focus_mode;
 
         // Calculate pixel size
         self.pixel_width = sensor_width / (self.resolution.0 as f32);
@@ -172,25 +215,101 @@ impl Camera {
         // Calculate position for first pixel
         self.first_pixel_pos = self.coordinate_system.origin
             + (self.distance_to_plane * self.coordinate_system.w)
-            - (self.coordinate_system.u * ((sensor_width / 2.0) - self.pixel_width / 2.0))
-            + (self.coordinate_system.v * ((sensor_height / 2.0) - self.pixel_height / 2.0));
+            - (self.coordinate_system.u * ((sensor_width / 2.0) - (self.pixel_width / 2.0)))
+            + (self.coordinate_system.v * ((sensor_height / 2.0) - (self.pixel_height / 2.0)));
+
+        return Ok(());
     }
 
     /// Cast a Ray to pixel (i, j)
-    pub fn cast_ray(&self, i: usize, j: usize) -> Option<Ray> {
+    fn cast_ray(&self, i: usize, j: usize, rng: &mut ThreadRng) -> Option<Ray> {
         if (i >= self.resolution.0) || (j >= self.resolution.1) {
             return None;
         }
 
+        let ray_origin = match self.focus_mode {
+            FocusMode::PinHole => self.coordinate_system.origin,
+            FocusMode::FocalPlane {
+                focal_distance: _,
+                aperture,
+            } => {
+                // Uniform sample of the aperture disc
+                let r: f32 = (aperture / 2.0) * rng.gen::<f32>().sqrt();
+                let phi: f32 = rng.gen_range(0.0..2.0 * PI);
+
+                // Distances from origin (within the disc)
+                let dx = r * phi.cos();
+                let dy = r * phi.sin();
+
+                self.coordinate_system.origin
+                    + dx * self.coordinate_system.u
+                    + dy * self.coordinate_system.v
+            }
+        };
+
         let pixel_position = self.first_pixel_pos
             + ((i as f32) * self.pixel_width * self.coordinate_system.u)
             - ((j as f32) * self.pixel_height * self.coordinate_system.v);
-        let ray_direction = pixel_position - self.coordinate_system.origin;
-        Some(Ray::new(self.coordinate_system.origin, ray_direction))
+        let ray_direction = pixel_position - ray_origin;
+
+        Some(Ray::new(ray_origin, ray_direction))
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use approx::assert_relative_eq;
+
     use super::*;
+
+    #[test]
+    fn sample_pinhole() {
+        let config = CameraConfig {
+            position: Vec3::zero(),
+            direction: algebra::UNIT_Z,
+            resolution: (800, 600),
+            rotation: 0.0_f32,
+            fov: FieldOfView::Horizontal(90f32.to_radians()),
+            focus_mode: FocusMode::PinHole,
+        };
+        let camera = Camera::new(&config);
+
+        let mut rng = rand::thread_rng();
+        for i in 0..800 {
+            for j in 0..600 {
+                let ray = camera.cast_ray(i, j, &mut rng);
+                assert_eq!(Vec3::zero(), ray.unwrap().origin());
+            }
+        }
+    }
+
+    #[test]
+    fn sample_aperture() {
+        let aperture: f32 = 0.1;
+        let config = CameraConfig {
+            position: Vec3::zero(),
+            direction: algebra::UNIT_Z,
+            resolution: (800, 600),
+            rotation: 0.0_f32,
+            fov: FieldOfView::Horizontal(90f32.to_radians()),
+            focus_mode: FocusMode::FocalPlane {
+                focal_distance: 1.0,
+                aperture,
+            },
+        };
+        let camera = Camera::new(&config);
+
+        let mut rng = rand::thread_rng();
+        for i in 0..800 {
+            for j in 0..600 {
+                let ray = camera.cast_ray(i, j, &mut rng);
+                let ray_origin = ray.unwrap().origin();
+                let x = ray_origin.x;
+                let y = ray_origin.y;
+                let z = ray_origin.z;
+                assert!((x.powf(2.0) + y.powf(2.0)).sqrt() <= (aperture / 2.0));
+                assert_relative_eq!(0.0, z);
+            }
+        }
+    }
 }
