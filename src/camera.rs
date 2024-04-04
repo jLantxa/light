b25e0a323ac3cd1535ffe72ed46045e0846628b1
Javier Lancha Vázquez <javier.lancha@gmail.com>
2024-04-04 23:51:39 +0200

    Fix camera config

--- src/camera.rs
+++ src/camera.rs
@@ -19,6 +19,7 @@
 
 use std::f32::consts::PI;
 
+use image::{Rgb, RgbImage};
 use rand::{rngs::ThreadRng, Rng};
 
 use crate::algebra::{self, Vec3, UNIT_Y};
@@ -52,7 +53,7 @@ impl Default for FocusMode {
 pub struct CameraConfig {
     pub position: Vec3,
     pub direction: Vec3,
-    pub resolution: (usize, usize),
+    pub resolution: (u32, u32),
     pub rotation: f32,
     pub fov: FieldOfView,
     pub focus_mode: FocusMode,
@@ -70,7 +71,7 @@ struct CoordinateSystem {
 pub struct Camera {
     coordinate_system: CoordinateSystem, // Coordinate system (origin and base vectors)
     rotation: f32,                       // Rotation, in the positive sense, around the facing axis
-    resolution: (usize, usize),          // Resolutions (width, height) in pixels
+    resolution: (u32, u32),              // Resolutions (width, height) in pixels
     fov: FieldOfView,                    // Field of view (Horizontal or Vertical) in radians
     focus_mode: FocusMode,
 
@@ -109,7 +110,7 @@ impl Camera {
         self.fov
     }
 
-    pub fn resolution(&self) -> (usize, usize) {
+    pub fn resolution(&self) -> (u32, u32) {
         self.resolution
     }
 
@@ -137,7 +138,7 @@ impl Camera {
                         "Vertical field of view must be in the interval (0, 180)".to_string()
                     );
                 }
-                self.fov = FieldOfView::Horizontal(alpha);
+                self.fov = FieldOfView::Vertical(alpha);
             }
         }
 
@@ -145,6 +146,7 @@ impl Camera {
         self.rotation = config.rotation;
 
         // Set coordinates
+        self.coordinate_system.origin = config.position;
         self.coordinate_system.w = config.direction.normal();
         self.coordinate_system.u = self.coordinate_system.w.cross(WORLD_UP).normal();
         self.coordinate_system.v = self
@@ -222,7 +224,7 @@ impl Camera {
     }
 
     /// Cast a Ray to pixel (i, j)
-    fn cast_ray(&self, i: usize, j: usize, rng: &mut ThreadRng) -> Option<Ray> {
+    fn cast_ray(&self, i: u32, j: u32, rng: &mut ThreadRng) -> Option<Ray> {
         if (i >= self.resolution.0) || (j >= self.resolution.1) {
             return None;
         }
@@ -253,6 +255,13 @@ impl Camera {
         let ray_direction = pixel_position - ray_origin;
 
         Some(Ray::new(ray_origin, ray_direction))
+    }
+
+    pub fn capture(&self, num_samples: u32) -> Result<RgbImage, String> {
+        let mut image = RgbImage::new(self.resolution.0, self.resolution.1);
+        let mut rng = rand::thread_rng();
+
+        todo!();
     }
 }
 
